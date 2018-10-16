b00691029852ec5ffbdc2210b8d555371821b499
Ryan Brady <ryanbrady369@gmail.com>
2018-10-16 20:25:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,10 +23,9 @@ def long_planeteer_calls(list)
 end
 
 def find_the_cheese(list)
-
+  cheese_types = ["cheddar", "gouda", "camembert"]
   list.each do |curr|
-    cheese_types = ["cheddar", "gouda", "camembert"]
-    if cheese_types.includes?(curr)
+    if cheese_types.include?(curr)
       return curr
     end
   end
