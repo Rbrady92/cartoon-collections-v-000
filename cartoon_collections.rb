bca80cf8c4e0345a0d97d19ab91250697180faed
Ryan Brady <ryanbrady369@gmail.com>
2018-10-16 20:04:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(list)
   count = 0
-  list.each do |curr| 
+  list.each do |curr|
     puts "#{count}.*#{curr}"
     count += 1
-  end 
+  end
 end
 
 def summon_captain_planet# code an argument here
